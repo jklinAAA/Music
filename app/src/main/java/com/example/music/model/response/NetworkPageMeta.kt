32d5fb01e8cca29bd04a278c98e16aa8e6982133
jklinAAA <3032728052@qq.com>
2024-08-03 19:36:32 +0800

    555

--- app/src/main/java/com/example/music/model/response/NetworkPageMeta.kt
+++ app/src/main/java/com/example/music/model/response/NetworkPageMeta.kt
@@ -1,4 +1,31 @@
 package com.example.music.model.response
 
-class NetworkPageMeta {
-}
+/**
+ * 分页模型
+ */
+data class NetworkPageMeta(
+    /**
+     * 有多少条
+     */
+    val total: Int? = null,
+
+    /**
+     * 有多少页
+     */
+    val pages: Int? = null,
+
+    /**
+     * 当前每页显示多少条
+     */
+    val size: Int? = null,
+
+    /**
+     * 当前页
+     */
+    val page: Int? = null,
+
+    /**
+     * 下一页
+     */
+    val next: Int? = null,
+)
